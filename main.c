3725815f1df49c245b4cdd79e4c33b2c600fe3e0
diana-helppls <dianarassylnyh@gmail.com>
2025-02-04 21:55:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     char first[255], last[255];
     askname(first, last);
-    printf("Hello, %s %s!\n", first, last);
+    printf("Hello, %s %s!\n", first, last); /*hihihih*/
     return 0;
     //here
 }
